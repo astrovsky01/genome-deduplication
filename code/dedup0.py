9f6f2356f8fa9ba76d491206c8594b120955efe5
mrevsin1 <mrevsin1@login02.cm.cluster>
2025-09-29 17:08:57 -0400

    Cleaned up and restructed the code

--- code/dedup0.py
+++ code/dedup0.py
@@ -98,7 +98,7 @@ def sample_scan(seq, start, end, k, seen_kmers, sample_seen_kmers, masked_starts
 
 ## Output Functions ================
 
-def writeout_bed(name, regions, bedfile):
+def writeout_bed(name, regions, bedfile, seq_len=None):
     """
     Create bed files for regions or just a list of start sites for beginning positions
     """
@@ -108,7 +108,10 @@ def writeout_bed(name, regions, bedfile):
                 f.write(f"{name}\t{start}\t{end}\n")
         else:
             for start in regions:
-                f.write(f"{name}\t{start}\n")
+                if seq_len is None:
+                    f.write(f"{name}\t{start}\n")
+                else:
+                    f.write(f"{name}\t{start}\t{start+seq_len}\n")
 
 def writeout_kmers(seen_kmer_set, outfile):
     """
@@ -121,9 +124,9 @@ def output_dump(local_dict, file_basename):
     """
     Output all bed files
     """
-    with open(file_basename + "_sample_regions.bed", 'w') as sample_regions:
-        with open(file_basename + "_masked_starts.bed", 'w') as masked_starts:
-            with open(file_basename + "_skipped_regions.bed", 'w') as skipped_regions:
+    with open(file_basename + ".samples.bed", 'w') as sample_regions:
+        with open(file_basename + ".masks.bed", 'w') as masked_starts:
+            with open(file_basename + ".ignored.bed", 'w') as skipped_regions:
                 for seqname in local_dict.keys():
                     writeout_bed(seqname, local_dict[seqname]["sample_regions"], sample_regions)
                     writeout_bed(seqname, local_dict[seqname]["masked_starts"], masked_starts)
@@ -170,11 +173,16 @@ def test_with_sequence(sequence, k, sample_len, seen_kmers):
 ### Main functions
 
 ## Main Deduplication ================
-def deduplicate(seq, k, sample_len, seen_kmers=None, min_sample_len=None):
+def deduplicate_seq(seq, args, seen_kmers):
+
+    # Collect needed args
+    k = args.k
+    sample_len = args.sample_len
+    min_sample_len = args.min_sample_len
+    retain_rate = args.retain
+    seed = args.seed
 
     # Check validity of inputs
-    if k < sample_len:
-        raise("Error: sample length cannot be greater than k.")
     if min_sample_len is not None and len(seq) < min_sample_len:
         raise("Error: the sequence length is less than min_sample_len.")
     if min_sample_len is None and len(seq) < sample_len:
@@ -275,6 +283,72 @@ def deduplicate(seq, k, sample_len, seen_kmers=None, min_sample_len=None):
     return sample_regions, masked_starts, skipped_regions, seen_kmers
 
 
+def deduplicate_genome(fasta, seen_kmers, args):
+
+    # Keep dict associating the regions with the sequence name
+    local_dict = {}
+
+    # Set output prefix
+    fasta_basename = ''.join(os.path.basename(fasta).split(".")[:-1])
+    out_prefix = os.path.join(args.output_dir, fasta_basename)
+
+    # Read in fasta
+    with open(fasta, 'r') as f:
+        for record in SeqIO.parse(f, "fasta"):
+            sequence = str(record.seq)
+            seqname = record.id
+
+            # Deduplicate this sequence
+            sample_regions, masked_starts, skipped_regions, seen_kmers = deduplicate(sequence, seen_kmers, args)
+
+            print(f"Sample regions: {sample_regions}")
+            print(f"Masked starts: {masked_starts}")
+            print(f"Skipped regions: {skipped_regions}")
+
+            # Associate deduplication data with the sequence name
+            local_dict[seqname] = {
+                "sample_regions": sample_regions,
+                "masked_starts": masked_starts,
+                "skipped_regions": skipped_regions,
+            }
+
+        output_dump(local_dict, out_prefix)
+
+    writeout_kmers(seen_kmers, file_basename + f".pickle")
+
+    print(f"Done with {fasta}")
+    return seen_kmers
+
+
+def deduplicate(args):
+
+    # Read input file of genome locations
+    with open(args.input, 'r') as f:
+        fastas = [line.rstrip('\n') for line in f.readlines()]
+
+    # Filter down to only the fastas that we can find and issue warning about
+    # any we can't find
+    valid_fastas = [fasta for fasta in fastas if os.path.isfile(fasta)]
+    invalid_fastas = list(set(fastas).difference(set(valid_fastas)))
+    if len(invalid_fastas) > 0:
+        print("Warning: could not find the following fastas:")
+        print(invalid_fastas)
+
+    # Initialize seen kmers here
+    if args.seen_kmers is None:
+        seen_kmers = set()
+    else:
+        print("Loading seen kmers")
+        seen_kmers = pickle.load(open(args.seen_kmers, "rb"))
+
+    # Iterate over fastas and deduplicate each one
+    for fasta in valid_fastas:
+
+        # Deduplicate this fasta
+        print(f"Deduplicating {fasta}")
+        seen_kmers = deduplicate_genome(fasta, seen_kmers, args)
+
+
 ###=============================================================================
 ### Call to main
 
@@ -282,34 +356,54 @@ def __main__():
 
     ## Collect input args
     parser = argparse.ArgumentParser()
-    parser.add_argument("fasta", help="Input FASTA file")
-    parser.add_argument("-k". "--kmer", type=int, default=32, help="Kmer size (default: 32)")
+    parser.add_argument("input", help="Input list of FASTA files")
+    parser.add_argument("-k", "--kmer", type=int, default=32, help="Kmer size (default: 32)")
     parser.add_argument("-l", "--sample_len", type=int, default=1000, help="Sample length (default: 1000)")
     parser.add_argument("-m", "--min_sample_len", type=int, default=50, help="Minimum sample length (default: 50)")
+    parser.add_argument("-o", "--output_dir", default="dedup_out", help="Output directory (default: dedup_out/)")
     parser.add_argument("-p", "--seen_kmers", default=None, help="Pickle file containing seen kmers (default: None)")
     parser.add_argument("-r", "--retain", type=float, default=0.0, help="Likelihood a duplicate kmer will be allowed through as a value from [0,1]")
     parser.add_argument("-seed", "--seed", type=int, default=123, help="Random seed for reproducibility")
     args = parser.parse_args()
 
     ## Process input args, checking for validity
-    fasta = args.fasta
-    input_basename = os.path.splitext(os.path.basename(fasta))[0]
-    k = args.k
-    sample_len = args.sample_len
-    min_sample_len = args.min_sample_len
-    seed = args.seed
-    rng = random.Random(seed)
-    if args.seen_kmers is not None:
-        seen_kmers = set(pickle.load(open(args.seen_kmers, "rb")))
-    else:
-        seen_kmers = set()
-    retain = args.retain
-    if retain is not None:
-        if retain > 1.0 or retain < 0.0:
+
+    # Input fasta must be a valid file
+    if not os.path.isfile(args.input):
+        raise("Error: could not find supplied list of fasta files")
+
+    # Kmer must be a positive number
+    if args.kmer < 1:
+        raise("Error: kmer size must be a positive integer")
+
+    # Seen kmers must be either none or a valid pickle file
+    if args.seen_kmers if not None:
+        if not os.path.isfile(args.seen_kmers):
+            raise("Error: could not find supplied seen kmers file")
+
+    # Retain rate must be between 0 and 1
+    if args.retain is not None and (args.retain > 1.0 or args.retain < 0.0):
             raise("Error: Retention rate of duplicate kmers cannot be greater than 1.0 or less than 0.0")
 
+    # If sample len is smaller than k, issue warning that no deduplication will occur. Not an error bc this can be a way to create the raw sample sets
+    if args.sample_len < args.k:
+        print("Warning: sample len is smaller than k, meaning no deduplication will occur")
+
+    # If min sample len is higher than sample len, set min to equal the default sample len
+    if args.min_sample_len > args.sample_len:
+        print("Warning: min sample length cannot be bigger than the default sample length; defaulting to equal the standard sample length")
+        args.min_sample_len = args.sample_len
+
+    # If kmer is very small, issue a warning
+    if args.kmer < 16:
+        print("Warning: small kmer sizes will result in very strict deduplication. Consider increasing the kmer size")
+
+    # Create output directory if it doesn't already exist
+    if not os.path.isdir(args.output_dir):
+        os.makedirs(args.output_dir)
+
     ## Run deduplication
-    deduplicate()
+    deduplicate(args)
 
 
 if __name__ == "__main__":
