4350c3d90f9db588b68d9931f3491e94cd2697e8
astrovsky01 <a.ostrovsky@mac.com>
2025-10-09 18:08:36 -0400

    debugging progress

--- code/aggregate_and_track_samples.sh
+++ code/aggregate_and_track_samples.sh
@@ -29,30 +29,43 @@ train_files=$(ls ${in_dir}/*.samples.train.bed)
 dev_files=$(ls ${in_dir}/*.samples.dev.bed)
 
 while IFS=$'\t' read -r var_name var_value; do
-    declare "$var_name=$var_value"
-done < basename_fasta_match.txt
+    # Replace dots with underscores to make valid bash variable names
+    clean_var_name="${var_name//\./_}"
+    declare "$clean_var_name=$var_value"
+done < ${in_dir}/basename_fasta_match.txt
 
 
 # Create combined train and test bed files with labeled origins
 for file in $train_files; do
     file_basename=$(basename "$file" .samples.train.bed)
-    cat "$file" | awk -v fname="$file_basename" '{print $0; print fname}' >> $train_bed
+    cat "$file" | awk -v fname="$file_basename" '{print $0 "\t" fname}' >> $train_bed
 done
 python code/shuffle.py $train_bed $random_seed $train_bed.shuf
 for file in $dev_files; do
-    cat "$file" | awk -v fname="$file_basename" '{print $0; print fname}' >> $dev_bed
+    file_basename=$(basename "$file" .samples.dev.bed)
+    cat "$file" | awk -v fname="$file_basename" '{print $0 "\t" fname}' >> $dev_bed
 done
 python code/shuffle.py $dev_bed $random_seed $dev_bed.shuf
 
-$train_samples = $(cat $train_bed)
-
 while IFS=$'\t' read -r chr start end sample_id; do
-    fasta_file="${!sample_id}"  # Get actual fasta file from name
-    echo -e "$chr\t$start\t$end" | bedtools getfasta -fi "$fasta_file" -bed - -tab | cut -f2
+    # Convert dots to underscores to match declared variable names
+    clean_sample_id="${sample_id//\./_}"
+    fasta_file="${!clean_sample_id}"  # Get actual fasta file from name
+    if [[ "$fasta_file" == *.gz ]]; then
+        echo -e "$chr\t$start\t$end" | bedtools getfasta -fi <(zcat "$fasta_file") -bed - -tab | cut -f2
+    else
+        echo -e "$chr\t$start\t$end" | bedtools getfasta -fi "$fasta_file" -bed - -tab | cut -f2
+    fi
 done < "$train_bed" >> "${train_txt}"
 
 
 while IFS=$'\t' read -r chr start end sample_id; do
-    fasta_file="${!sample_id}"
-    echo -e "$chr\t$start\t$end" | bedtools getfasta -fi "$fasta_file" -bed - -tab | cut -f2
+    # Convert dots to underscores to match declared variable names
+    clean_sample_id="${sample_id//\./_}"
+    fasta_file="${!clean_sample_id}"
+    if [[ "$fasta_file" == *.gz ]]; then
+        echo -e "$chr\t$start\t$end" | bedtools getfasta -fi <(zcat "$fasta_file") -bed - -tab | cut -f2
+    else
+        echo -e "$chr\t$start\t$end" | bedtools getfasta -fi "$fasta_file" -bed - -tab | cut -f2
+    fi
 done < "$dev_bed" >> "${dev_txt}"
