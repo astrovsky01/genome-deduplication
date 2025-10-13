59e25db81e72208eaa17ebe6477475790eff1bc2
astrovsky01 <a.ostrovsky@mac.com>
2025-10-13 16:54:22 -0400

    Confirmation before overwritin

--- code/get_samples.sh
+++ code/get_samples.sh
@@ -45,6 +45,26 @@ if [ -z "$data_folder" ] || [ -z "$dev_pct" ] || [ -z "$max_dev_samples" ] || [ 
 fi
 
 
+#Check if paritioned files exist already and check for confirmation to overwrite
+if ls ${data_folder}/*.samples.dev.bed 1> /dev/null 2>&1 || ls ${data_folder}/*.samples.train.bed 1> /dev/null 2>&1; then
+    read -p "Partitioned sample files already exist. Do you want to overwrite them? (y/n) " choice
+    case "$choice" in
+        y|Y ) echo "Overwriting existing partitioned sample files..." && rm ${data_folder}/*.samples.dev.bed ${data_folder}/*.samples.train.bed;;
+        n|N ) echo "Exiting script."; exit 1;;
+        * ) echo "Invalid choice. Exiting script."; exit 1;;
+    esac
+fi
+
+#Check if final output files exist already and check for confirmation to overwrite
+if [ -f "${data_folder}/all_train.bed" ] || [ -f "${data_folder}/all_dev.bed" ]; then
+    read -p "Combined and shuffled files already exist. Do you want to overwrite them? (y/n) " choice
+    case "$choice" in
+        y|Y ) echo "Overwriting existing files..." && rm "${data_folder}/all_train.bed" "${data_folder}/all_dev.bed";;
+        n|N ) echo "Exiting script."; exit 1;;
+        * ) echo "Invalid choice. Exiting script."; exit 1;;
+    esac
+fi
+
 ## Create train/dev txt files for each sample in the input directory
 
 # Get all samples.bed files in the input directory
@@ -58,11 +78,10 @@ for sample_bed in $sample_beds; do
 	sample_fasta=$(grep "^${basename}" ${data_folder}/basename_fasta_match.txt | cut -f2) # query corresponding fasta for this sample from basename_fasta_match.txt
 	dev_sample_bed=${data_folder}/${basename}.samples.dev.bed
 	train_sample_bed=${data_folder}/${basename}.samples.train.bed
-	# dev_sample_txt=${data_folder}/${basename}.samples.dev.txt
-	# train_sample_txt=${data_folder}/${basename}.samples.train.txt
 
 	# Get training/dev beds/txts for each sample bed
 	echo "Partitioning samples"
 	./code/partition_samples.sh $sample_bed $dev_pct $max_dev_samples $shuf_seed
 done
+echo "Finished partitioning samples into train/dev sets. Aggregating and retrieving sequences now"
 ./code/aggregate_and_track_samples.sh $data_folder $shuf_seed 
