15b34be5b3672c273ce3aed841aa9901993a4505
mrevsin1 <mrevsin1@devschatz1.cm.cluster>
2025-10-15 16:59:58 -0400

    Updated to allow overlaps shorter than k

--- code/check_beds.sh
+++ code/check_beds.sh
@@ -3,27 +3,36 @@
 ### Usage: ./code/check_beds.sh [output dir from dedup0.py] [sample prefix (everything before the .samples.bed)]
 ### e.g. ./code/check_beds.sh tests/out/new_no_overlap/ GCA_001775245.1_ASM177524v1_genomic
 
+module load jq
+
 # Input args
 data_folder=$1
 sample=$2
 
-### Check 4 - beds are fully disjoint
+### Check 4 - beds are fully disjoint, notwithstanding permitted overlaps
+
+allowed_overlap_size=0
+if [[ "$(jq -r '.no_overlap' $data_folder/config.json)" == "false" ]]; then
+	kmer_size=$(jq -r '.kmer' $data_folder/config.json)
+	allowed_overlap_size=$((kmer_size - 1))
+fi
+
 bed_types=("samples" "masks" "ignored" "ambiguous")
 n_bed_types=${#bed_types[@]}
 success=1
 for ((i=1; i<$n_bed_types; i++)); do
 	for ((j=0; j<$i; j++)); do
-		bed_A=$data_folder/${sample}.${bed_types[$i]}.bed
-		bed_B=$data_folder/${sample}.${bed_types[$j]}.bed
-		n_overlaps=$(bedtools intersect -a $bed_A -b $bed_B | wc -l)
-		if [[ $n_overlaps -gt 0 ]]; then
+		bed_A=$data_folder/${sample}.${bed_types[$j]}.bed
+		bed_B=$data_folder/${sample}.${bed_types[$i]}.bed
+		max_overlap_size=$(bedtools intersect -a $bed_A -b $bed_B | awk '{print $3-$2}' | sort -n | uniq | tail -1)
+		if [[ $max_overlap_size -gt $allowed_overlap_size ]]; then
 			success=0
-        		echo "Check 4 failed: ${bed_types[$i]} and ${bed_types[$j]} bed files have $n_overlaps overlapping regions"
+        		echo "Check 4 failed: ${bed_types[$j]} and ${bed_types[$i]} bed files have at least one overlapping region of size $max_overlap_size"
 		fi
 	done
 done
 if [[ $success -eq 1 ]]; then
-	echo "Check 4 complete! All bed files are disjoint"
+	echo "Check 4 complete! All bed files are appropriately disjoint"
 fi
 
 ### Check 5 - beds cover the entirety of the original fasta
