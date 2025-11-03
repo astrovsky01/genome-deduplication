71e41773025e6c4da1446e780ea36e1efbd54f1f
astrovsky01 <aostrov4@devschatz1.cm.cluster>
2025-11-03 15:19:10 -0500

    tested and functional

--- code/summary_stats/generate_summary_stats.sh
+++ code/summary_stats/generate_summary_stats.sh
@@ -9,15 +9,17 @@ mem=$4
 threads=$5
 
 mkdir -p $output_dir
+if [ -e ${output_dir}/summary_stats.txt ]; then
+    rm ${output_dir}/summary_stats.txt
+fi
 touch ${output_dir}/summary_stats.txt
 
-source_kmer_counts="/source_data_kmer_counts.txt"
+source_kmer_counts="/source_fasta_kmer_counts.txt"
 deduped_file="/deduped_kmer_counts.txt"
 ignored_file="/ignored_kmer_counts.txt"
 ignored_and_deduped_file="/ignored_deduped_kmer_counts.txt"
 masked_file="/masked_kmer_counts.txt"
 combined_file="/combined_all_kmers.txt"
-
 
 
 #generate dedup kmer counts
@@ -29,44 +31,44 @@ mv ${input_dir}${combined_file} ${output_dir}${combined_file}
 mv ${input_dir}${ignored_and_deduped_file} ${output_dir}${ignored_and_deduped_file}
 
 #Generate source data kmer counts
-sh code/summary_stats/kmer_counter_source_data.sh $input_dir $kmer_size $mem $threads
+sh code/summary_stats/kmer_counter_original_files.sh $input_dir $kmer_size $mem $threads
 mv ${input_dir}${source_kmer_counts} ${output_dir}${source_kmer_counts}
 
 
 #Generate kmer count statistics
-unique_source_kmers=$(wc -l ${output_dir}${source_kmer_counts})
+unique_source_kmers=$(wc -l < ${output_dir}${source_kmer_counts})
 source_total_kmer_count=$(awk '{sum+=$2} END{print sum}' ${output_dir}${source_kmer_counts})
 
-unique_deduped_kmers=$(wc -l ${output_dir}${deduped_file})
+unique_deduped_kmers=$(wc -l < ${output_dir}${deduped_file})
 deduped_total_kmer_count=$(awk '{sum+=$2} END{print sum}' ${output_dir}${deduped_file})
 
-unique_ignored_kmers=$(wc -l ${output_dir}${ignored_file})
+unique_ignored_kmers=$(wc -l < ${output_dir}${ignored_file})
 ignored_total_kmer_count=$(awk '{sum+=$2} END{print sum}' ${output_dir}${ignored_file})
 
-unique_ignored_deduped_kmers=$(wc -l ${output_dir}${ignored_and_deduped_file})
+unique_ignored_deduped_kmers=$(wc -l < ${output_dir}${ignored_and_deduped_file})
 deduped_ignored_total_kmer_count=$(awk '{sum+=$2} END{print sum}' ${output_dir}${ignored_and_deduped_file})
 
-unique_masked_kmers=$(wc -l ${output_dir}${masked_file})
+unique_masked_kmers=$(wc -l < ${output_dir}${masked_file})
 masked_total_kmer_count=$(awk '{sum+=$2} END{print sum}' ${output_dir}${masked_file})
 
-unique_combined_kmers=$(wc -l ${output_dir}${combined_file})
+unique_combined_kmers=$(wc -l < ${output_dir}${combined_file})
 combined_total_kmer_count=$(awk '{sum+=$2} END{print sum}' ${output_dir}${combined_file})
 
 
 #Write summary stats to file
 echo -e "Summary Statistics for kmer size ${kmer_size}:\n" >> ${output_dir}/summary_stats.txt
-echo -e "Source\tUnique kmer counts:\tTotal kmer counts:\n" >> ${output_dir}/summary_stats.txt
-echo -e "${unique_source_kmers}\t${source_total_kmer_count}\n" >> ${output_dir}/summary_stats.txt
-echo -e "${unique_deduped_kmers}\t${deduped_total_kmer_count}\n" >> ${output_dir}/summary_stats.txt
-echo -e "${unique_ignored_kmers}\t${ignored_total_kmer_count}\n" >> ${output_dir}/summary_stats.txt
-echo -e "${unique_ignored_deduped_kmers}\t${deduped_ignored_total_kmer_count}\n" >> ${output_dir}/summary_stats.txt
-echo -e "${unique_masked_kmers}\t${masked_total_kmer_count}\n" >> ${output_dir}/summary_stats.txt
-echo -e "${unique_combined_kmers}\t${combined_total_kmer_count}\n" >> ${output_dir}/summary_stats.txt
+echo -e "Source\t|\tUnique kmer counts:\t|\tTotal kmer counts:\n" >> ${output_dir}/summary_stats.txt
+echo -e "Original fasta files\t|\t${unique_source_kmers}\t|\t${source_total_kmer_count}\n" >> ${output_dir}/summary_stats.txt
+echo -e "Deduplicated files\t|\t${unique_deduped_kmers}\t|\t${deduped_total_kmer_count}\n" >> ${output_dir}/summary_stats.txt
+echo -e "Ignored files\t|\t${unique_ignored_kmers}\t|\t${ignored_total_kmer_count}\n" >> ${output_dir}/summary_stats.txt
+echo -e "Ignored and deduplicated files\t|\t${unique_ignored_deduped_kmers}\t|\t${deduped_ignored_total_kmer_count}\n" >> ${output_dir}/summary_stats.txt
+echo -e "Masked files\t|\t${unique_masked_kmers}\t|\t${masked_total_kmer_count}\n" >> ${output_dir}/summary_stats.txt
+echo -e "Combined files\t|\t${unique_combined_kmers}\t|\t${combined_total_kmer_count}\n" >> ${output_dir}/summary_stats.txt
 
 
 
 #Show kmer counts of original dataset overlayed with deduped and deduped+ignored kmer distributions
-python code/summary_stats/kmer_count_summary.py ${output_dir}
+# python code/summary_stats/kmer_count_summary.py ${output_dir}
 
 #Show where kmers came from in deduped datasets
-python code/summary_stats/kmer_distribution_plot.py ${input_dir} ${output_dir}
+# python code/summary_stats/kmer_distribution_plot.py ${input_dir} ${output_dir}
